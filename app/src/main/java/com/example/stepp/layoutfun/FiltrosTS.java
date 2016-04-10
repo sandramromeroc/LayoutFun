4e4b0ebb78cdd30cd5b9b18b51e4c35b6d85f959
sandra <sandrar20@hotmail.com>
2016-04-10 18:10:45 -0500

    Commit

---
+++ app/src/main/java/com/example/stepp/layoutfun/FiltrosTS.java
@@ -0,0 +1,22 @@
+package com.example.stepp.layoutfun;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FiltrosTS extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filtros_ts);
+    }
+
+    public void click(View View){
+        Intent i = new Intent(this, com.example.stepp.layoutfun.Resultados.class);
+        //i.putExtra("direccion", et1.getText().toString());
+        startActivity(i);
+
+    }
+}
--- app/src/main/java/com/example/stepp/layoutfun/MainActivity.java
+++ app/src/main/java/com/example/stepp/layoutfun/MainActivity.java
@@ -7,6 +7,7 @@
 package com.example.stepp.layoutfun;
 //
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,70 +18,20 @@ public class MainActivity extends Activity {
      * This method is called when the user chooses one of the turtle radio buttons.
      * In this code we set which turtle image is visible on the screen in the ImageView.
      */
-    public void pickTurtle(View view) {
-        ImageView img = (ImageView) findViewById(R.id.sombrero);
-        if (view.getId() == R.id.leo) {
-            img.setImageResource(R.drawable.hat);
-        }
-    }
-    public void brazos(View view) {
-        ImageView img = (ImageView) findViewById(R.id.brazos);
-        if (view.getId() == R.id.mike) {
-            img.setImageResource(R.drawable.arms);
-        }
-    }
-    public void orejas(View view) {
-        ImageView img = (ImageView) findViewById(R.id.orejas);
-        if (view.getId() == R.id.don) {
-            img.setImageResource(R.drawable.ears);
-        }
-    }
-    public void nariz(View view) {
-        ImageView img = (ImageView) findViewById(R.id.nariz2);
-        if (view.getId() == R.id.nariz) {
-            img.setImageResource(R.drawable.nose);
-        }
-    }
-    public void cejas(View view) {
-        ImageView img = (ImageView) findViewById(R.id.cejas2);
-        if (view.getId() == R.id.cejas) {
-            img.setImageResource(R.drawable.eyebrows);
-        }
-    }
-    public void ojos(View view) {
-        ImageView img = (ImageView) findViewById(R.id.ojos2);
-        if (view.getId() == R.id.ojos) {
-            img.setImageResource(R.drawable.eyes);
-        }
-    }
-    public void gafas(View view) {
-        ImageView img = (ImageView) findViewById(R.id.gafas2);
-        if (view.getId() == R.id.gafas) {
-            img.setImageResource(R.drawable.glasses);
-        }
-    }
-    public void sonrrisa(View view) {
-        ImageView img = (ImageView) findViewById(R.id.sonrrisa2);
-        if (view.getId() == R.id.risa) {
-            img.setImageResource(R.drawable.mouth);
-        }
-    }
-    public void mostacho(View view) {
-        ImageView img = (ImageView) findViewById(R.id.bigote2);
-        if (view.getId() == R.id.bigote) {
-            img.setImageResource(R.drawable.mustache);
-        }
-    }
-    public void tillas(View view) {
-        ImageView img = (ImageView) findViewById(R.id.tillas2);
-        if (view.getId() == R.id.tillas) {
-            img.setImageResource(R.drawable.shoes);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void click(View View){
+        Intent i = new Intent(this, com.example.stepp.layoutfun.FiltrosTS.class);
+        //i.putExtra("direccion", et1.getText().toString());
+        startActivity(i);
+
+    }
+
+
 }
